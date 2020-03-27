00915881f8412ac4caf2837f4f91b256ba46633b
ubuntu <ubuntu@localhost.localdomain>
2020-03-27 21:05:23 +0800

    test

--- scratch/test.cc
+++ scratch/test.cc
@@ -1,22 +1,24 @@
-using namespace ns3;
 
 #include <ns3/wifi-module.h>
 #include <ns3/csma-module.h>
 #include <ns3/core-module.h>
+
+using namespace ns3;
+
 int
 main (int argc, char *argv[])
 {
 	
 	Ptr<WifiNetDevice> dev1 = CreateObject<WifiNetDevice> ();
-	cout << "dev1: " << dev1->GetTypeId().GetGroupName() <<endl;
+	std::cout << "dev1: " << dev1->GetTypeId().GetGroupName() <<std::endl;
 	Ptr<CsmaNetDevice> dev2 = CreateObject<CsmaNetDevice> ();
-	cout << "dev2: " << dev2->GetTypeId().GetGroupName() <<endl;
+	std::cout << "dev2: " << dev2->GetTypeId().GetGroupName() <<std::endl;
 	NetDeviceContainer con;
 	con.Add (dev1);
 	con.Add (dev2);
-	cout << "in container:" <<endl;
-	cout << "dev1: " << con.Get(0)->GetTypeId().GetGroupName() <<endl;
-	cout << "dev2: " << con.Get(1)->GetTypeId().GetGroupName() <<endl;
+	std::cout << "in container:" <<std::endl;
+	std::cout << "dev1: " << con.Get(0)->GetTypeId().GetGroupName() <<std::endl;
+	std::cout << "dev2: " << con.Get(1)->GetTypeId().GetGroupName() <<std::endl;
 	
 	return 0;
 }
