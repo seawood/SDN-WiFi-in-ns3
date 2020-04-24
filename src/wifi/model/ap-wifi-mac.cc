8eb50b1444ca4e20a63466e4ae061cc3b4532336
ubuntu <ubuntu@localhost.localdomain>
2020-04-24 09:45:47 +0800

    bug

--- src/wifi/model/ap-wifi-mac.cc
+++ src/wifi/model/ap-wifi-mac.cc
@@ -212,16 +212,16 @@ ApWifiMac::AssocSTA(const Mac48Address& from, Packet pkt)
 {
 	NS_LOG_FUNCTION(this);
 	NS_LOG_DEBUG ("Associate STA controller config:" << from);
-	m_staMgtAssocReqHeaders[from] = pkt;
-	MgtAssocRequestHeader assocReq;
-	NS_LOG_INFO("1");
-	pkt.RemoveHeader(assocReq);
-	NS_LOG_INFO("2");
 	if (m_staMgtAssocReqHeaders.find(from) != m_staMgtAssocReqHeaders.end())
 	{
 		NS_LOG_ERROR("Associate STA controller config failed: alerady associated");
 		return 1;
 	}
+	m_staMgtAssocReqHeaders[from] = pkt;
+	MgtAssocRequestHeader assocReq;
+	NS_LOG_INFO("1");
+	pkt.RemoveHeader(assocReq);
+	NS_LOG_INFO("2");
 	CapabilityInformation capabilities = assocReq.GetCapabilities ();
 	m_stationManager->AddSupportedPlcpPreamble (from, capabilities.IsShortPreamble ());
 	SupportedRates rates = assocReq.GetSupportedRates ();
