131995b05dffd89ae2fbc520e75b8cc775839079
ubuntu <ubuntu@localhost.localdomain>
2020-05-04 22:38:27 +0800

    bug

--- scratch/load-balance-RSSI.cc
+++ scratch/load-balance-RSSI.cc
@@ -149,7 +149,7 @@ main (int argc, char *argv[])
 
 	// Use the CsmaHelper to connect AP nodes to the switch node
 	CsmaHelper csmaHelper;
-	csmaHelper.SetChannelAttribute ("DataRate", DataRateValue (DataRate ("100Mbps")));
+	csmaHelper.SetChannelAttribute ("DataRate", DataRateValue (DataRate ("5Mbps")));
 	csmaHelper.SetChannelAttribute ("Delay", TimeValue (MilliSeconds (2)));
 	NetDeviceContainer apDevices;
 	NetDeviceContainer switchPorts;
@@ -159,7 +159,7 @@ main (int argc, char *argv[])
 	apDevices.Add (link1.Get (0));
 	switchPorts.Add (link1.Get (1));
 	
-	csmaHelper.SetChannelAttribute ("DataRate", DataRateValue (DataRate ("200Mbps")));
+	csmaHelper.SetChannelAttribute ("DataRate", DataRateValue (DataRate ("10Mbps")));
 	NodeContainer pair2 (aps.Get(1), switchNode);
 	NetDeviceContainer link2 = csmaHelper.Install (pair2);
 	apDevices.Add (link2.Get (0));
@@ -226,8 +226,8 @@ main (int argc, char *argv[])
 	positionAlloc->Add (Vector (distance*3, distance, 0.0));
 	positionAlloc->Add (Vector (0.0, 0.0, 0.0));
 	positionAlloc->Add (Vector (distance*0.2, 0.0, 0.0));
-	positionAlloc->Add (Vector (distance, 0.4, 0.0));
-	positionAlloc->Add (Vector (distance*0.6, 0.0, 0.0));
+	positionAlloc->Add (Vector (distance*0.5, 0.0, 0.0));
+	positionAlloc->Add (Vector (distance*0.8, 0.0, 0.0));
 	positionAlloc->Add (Vector (distance, 0.0, 0.0));
 	mobility.SetPositionAllocator (positionAlloc);
 	mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
@@ -275,7 +275,7 @@ main (int argc, char *argv[])
 	apps_sink.Start (Seconds (1));
 	apps_sink.Stop (Seconds (simTime + 1));	
 	OnOffHelper onoff ("ns3::UdpSocketFactory", InetSocketAddress (hostIpIfaces.GetAddress(0), port));
-	onoff.SetConstantRate (DataRate ("2Mb/s"), packetSize);
+	onoff.SetConstantRate (DataRate ("1Mb/s"), packetSize);
 	onoff.SetAttribute ("StartTime", TimeValue (Seconds (16)));
 	onoff.SetAttribute ("StopTime", TimeValue (Seconds (simTime+1)));
 	ApplicationContainer apps_source = onoff.Install (stas);
